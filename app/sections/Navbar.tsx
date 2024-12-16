979a96906d50b381d40e89d5d2a0a6667132f1d2
sneha-jhaveri <sneha@nivamoney.com>
2024-12-16 19:41:21 +0530

    content updation

--- app/sections/Navbar.tsx
+++ app/sections/Navbar.tsx
@@ -36,7 +36,7 @@ const Navbar: React.FC = () => {
                   priority
                 />
               </Link>
-              <span className="text-lg font-bold text-black">BotWot ICX</span>
+              <span className="text-lg font-bold text-black">BotWot</span>
             </div>
 
             {/* Mobile menu toggle */}
