5488ee0cf2da3d8c9196f042c45ed16d5d3c5926
sneha-jhaveri <sneha@nivamoney.com>
2024-12-29 17:17:22 +0530

    build bug fixed

--- app/products/channels/page.tsx
+++ app/products/channels/page.tsx
@@ -1,16 +1,5 @@
-import Hero from "../../components/HeroSection";
-import ProductSection from "../../components/ProductCard";
-import { channelsContent } from "../../data/products";
-
 const ChannelsPage = () => {
-  return (
-    <>
-      <div className="mt-10">
-        <Hero />
-        <ProductSection data={channelsContent.content} />
-      </div>
-    </>
-  );
+  return <></>;
 };
 
 export default ChannelsPage;
--- app/products/data-security/page.tsx
+++ app/products/data-security/page.tsx
@@ -1,14 +1,9 @@
-import Hero from "../../components/HeroSection";
-import ProductSection from "../../components/ProductCard";
-import { dataSecurityContent } from "../../data/products";
+
 
 const DataSecurityPage = () => {
   return (
     <>
-      <div className="mt-10">
-        <Hero />
-        <ProductSection data={dataSecurityContent.content} />
-      </div>
+    
     </>
   );
 };
