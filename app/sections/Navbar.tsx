f56c3d84b607f57c09283556884f499649debbd7
sneha-jhaveri <sneha@nivamoney.com>
2025-02-06 23:13:09 +0530

    updated links

--- app/sections/Navbar.tsx
+++ app/sections/Navbar.tsx
@@ -142,11 +142,16 @@ const Navbar: React.FC = () => {
                 </li>
               ))}
               <li>
-                <Link
-                  href="/signin"
+                {/* <Link
+                  href="https://botwot-icxapp.netlify.app/"
                   className="px-6 py-2 border-2 text-black border-[#387D8C] rounded-full hover:bg-green-100 hover:text-black transition-colors whitespace-nowrap"
                 >
                   Sign in
+                </Link> */}
+                <Link href="https://botwot-icxapp.netlify.app/" passHref>
+                  <button className="px-6 py-2 border-2 text-black border-[#387D8C] rounded-full hover:bg-green-100 hover:text-black transition-colors whitespace-nowrap">
+                    Sign in
+                  </button>
                 </Link>
 
                 <Link href="https://calendly.com/botwot62/30min" passHref>
@@ -202,11 +207,10 @@ const Navbar: React.FC = () => {
                 </div>
               ))}
               <div className="px-4 pt-4 flex flex-col space-y-3">
-                <Link
-                  href="/signin"
-                  className="px-4 py-2 text-center text-black border border-[#387D8C] rounded-full hover:bg-green-50"
-                >
-                  Sign in
+                <Link href="https://botwot-icxapp.netlify.app/" passHref>
+                  <button className="px-4 py-2 text-center text-black border border-[#387D8C] rounded-full hover:bg-green-50">
+                    Sign in
+                  </button>
                 </Link>
                 <Link
                   href="https://calendly.com/botwot62/30min"
